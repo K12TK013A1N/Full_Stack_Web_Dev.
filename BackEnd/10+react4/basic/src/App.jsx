1369f9b7f7630a66c967f2d260e8469f9d18b8dc
K12TK013A1N <shourya15singh7@gmail.com>
2024-08-27 16:24:55 +0530

    basic setup of react learned.

---
+++ BackEnd/10+react4/basic/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  var greeting = "GoodMorning"
+  var date = new Date();
+  var time = date.getTime();
+  var theme = {
+    color : "red",
+  }
+  if(time>12){
+    greeting = "GoodAfterNoon";
+    theme.color = "green";
+  }else if(time >18 && time <23){
+    greeting = "GoodEvening";
+    theme.color = "blue";
+  }
+  var obj = <h1 style={theme}>{greeting}</h1>
+  return obj
+}
+
+export default App
